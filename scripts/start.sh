95877768e61b7c750d39333c82a73dea292720d2
Balázs Varga <warnyul@gmail.com>
2020-03-21 15:13:48 +0100

    Deploy treafik to docker stack

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,11 @@
-env $(cat .dev.env) docker-compose build
-env $(cat .dev.env) docker-compose up -d
+#!/usr/bin/env sh
+
+# Variables
+BASEDIR=$(dirname "$0")
+
+set -a # automatically export all variables
+source .env
+source .dev.env
+set +a
+
+docker stack deploy bvarga-proxy -c ${BASEDIR}/../docker-compose.yml
